0e816a18bd3bcc3b7aef501e4eb301dabb82f843
gunayyz <100687596+gunayyz@users.noreply.github.com>
2022-03-23 22:54:24 +0400

    Add files via upload

---
+++ hw23/hw3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hw2
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int num = 35;
+            Square(ref num);
+            Console.WriteLine(num);
+        }
+        static int Square(ref int num)
+        {
+            int pow = 0;
+            for (int i = 1; i * i <= num; i++)
+            {
+                pow++;
+            }
+            num = pow;
+            return num;
+        }
+    }
+}
